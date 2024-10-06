339dfd0295977f9a9b31a831f0ef30514d11e449
adhavswapna <106309276+adhavswapna@users.noreply.github.com>
2024-10-06 15:45:16 +0530

    Update routes.tf

--- routes.tf
+++ routes.tf
@@ -6,7 +6,7 @@ resource "aws_route_table" "public-eks-route" {
     gateway_id = aws_internet_gateway.eks-igw.id
   }
 
-    tags = {
+  tags = {
     Name = "public-eks-route"
   }
 }
@@ -19,27 +19,19 @@ resource "aws_route_table" "private-eks-route" {
     gateway_id = aws_nat_gateway.eks_nat.id
   }
 
-    tags = {
+  tags = {
     Name = "private-eks-route"
   }
 }
 
-resource "aws_route_table_association" "RTA1" {
-  subnet_id      = aws_subnet.eks-public_subnet1.id
+# Public Subnet Route Table Association
+resource "aws_route_table_association" "RTA_public" {
+  subnet_id      = aws_subnet.eks-public_subnet.id
   route_table_id = aws_route_table.public-eks-route.id
 }
 
-resource "aws_route_table_association" "RTA2" {
-  subnet_id      = aws_subnet.eks-public_subnet2.id
-  route_table_id = aws_route_table.public-eks-route.id
-}
-
-resource "aws_route_table_association" "RTA3" {
-  subnet_id      = aws_subnet.eks_private_subnet1.id
-  route_table_id = aws_route_table.private-eks-route.id
-}
-
-resource "aws_route_table_association" "RTA4" {
-  subnet_id      = aws_subnet.eks_private_subnet2.id
+# Private Subnet Route Table Association
+resource "aws_route_table_association" "RTA_private" {
+  subnet_id      = aws_subnet.eks-private_subnet.id
   route_table_id = aws_route_table.private-eks-route.id
 }
